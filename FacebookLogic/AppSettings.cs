46e5a6bca7f5204e13924ec0871403d842f2e300
Eden Binyamin <edenbi@mta.ac.il>
2020-08-16 12:44:04 +0300

    logic finished

---
+++ FacebookLogic/AppSettings.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FacebookLogic
+{
+    public sealed class AppSettings
+    {
+        private static AppSettings s_Instance = null;
+        private readonly int r_MaxPostsAvailableToSee = 15;
+        private readonly string r_AppID = "322171932313614";
+        private readonly string[] r_UserPermissions =
+            {
+                "public_profile", "user_gender", "user_birthday", "user_hometown", "user_age_range", "user_likes",
+                "user_photos", "user_posts", "user_friends", "user_location", "user_tagged_places", "publish_to_groups",
+                "groups_access_member_info", "publish_pages"
+            };
+
+        private bool m_RememberUser;
+        private string m_LastAccessToken;
+
+        private AppSettings()
+        {
+            m_RememberUser = false;
+            m_LastAccessToken = string.Empty;
+        }
+        public static AppSettings Instance
+        {
+            get
+            {
+                if (s_Instance == null)
+                {
+                    s_Instance = new AppSettings();
+                }
+                else
+                {
+                    return s_Instance;
+                }
+                return s_Instance;
+            }
+        }
+
+        public int MaxPostsShown
+        {
+            get
+            {
+                return r_MaxPostsAvailableToSee;
+            }
+        }
+
+        public string AppID
+        {
+            get
+            {
+                return r_AppID;
+            }
+        }
+
+        public string[] UserPermissions
+        {
+            get
+            {
+                return r_UserPermissions;
+            }
+        }
+
+        public bool RememberUser
+        {
+            get
+            {
+                return m_RememberUser;
+            }
+
+            set
+            {
+                m_RememberUser = value;
+            }
+        }
+
+        public string LastAccessToken
+        {
+            get
+            {
+                return m_LastAccessToken;
+            }
+
+            set
+            {
+                m_LastAccessToken = value;
+            }
+        }
+    }
+}
---
+++ FacebookLogic/LoginManager.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace FacebookLogic
+{
+    class LoginManager
+    {
+        private static readonly string sr_ErrorLoginMassage = "Error: Failed to login. Please try again";
+        private static readonly string sr_ErrorInvalidAccessTokenMassage = "Error: Invalid access token. Please try different access token";
+        private static LoginManager s_Instance = null;
+        private readonly AppSettings r_AppSettings;
+        private LoginResult m_LoginResult;
+        private User m_LoggedInUser;
+
+        private LoginManager()
+        {
+            m_LoginResult = null;
+            m_LoggedInUser = null;
+            r_AppSettings = AppSettings.Instance;
+        }
+
+        public static LoginManager Instance
+        {
+            get
+            {
+                if (s_Instance == null)
+                {
+                    s_Instance = new LoginManager();
+                }
+
+                return s_Instance;
+            }
+        }
+
+        private void saveLoginData(LoginResult i_LoginResult)
+        {
+            m_LoginResult = i_LoginResult;
+            m_LoggedInUser = i_LoginResult.LoggedInUser;
+        }
+
+        public User LoggedInUser
+        {
+            get
+            {
+                return m_LoggedInUser;
+            }
+
+            set
+            {
+                m_LoggedInUser = value;
+            }
+        }
+
+        public bool IsLoggedIn
+        {
+            get
+            {
+                return m_LoginResult != null; 
+            }
+        }
+
+        public void Login()
+        {
+            LoginResult result;
+            try
+            {
+                result = FacebookService.Login(r_AppSettings.AppID, r_AppSettings.UserPermissions);
+                if (!string.IsNullOrEmpty(result.AccessToken))
+                {
+                    saveLoginData(result);
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception(sr_ErrorLoginMassage);
+            }
+        }
+
+        public void Logout()
+        {
+            FacebookWrapper.FacebookService.Logout(logoutSuccessful);
+        }
+
+        private void logoutSuccessful()
+        {
+            onLogoutSuccessful(EventArgs.Empty);
+        }
+
+        public void Connect(string i_AccessToken)
+        {
+            try
+            {
+                LoginResult res = FacebookService.Connect(i_AccessToken);
+                if (!string.IsNullOrEmpty(res.AccessToken))
+                {
+                    saveLoginData(res);
+                }
+            }
+            catch (Exception)
+            {
+                throw new Exception(sr_ErrorInvalidAccessTokenMassage);
+            }
+        }
+
+        private void onLogoutSuccessful(EventArgs e)
+        {
+            EventHandler handler = LogoutSuccessful;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event EventHandler LogoutSuccessful;
+    }
+}
+}
